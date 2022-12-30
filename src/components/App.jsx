d4b1ee9c28f17b4adae93808c9e594b917df0e82
Santi Soifer <santisoi2008@gmail.com>
2022-12-29 21:36:55 -0300

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TopButton from "./TopButton";
+import WaterCounter from "./WaterCounter";
+
+
+function App(props){
+    return (
+    <div>
+        <TopButton link="#" icon="User"/>
+        <TopButton link="#" icon="Settings"/>
+        <WaterCounter />
+    </div>
+    );
+} 
+
+export default App;
---
+++ src/components/TopButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TopButton(props){
+    return <a href={props.link}>{props.icon}</a>
+}
+
+export default TopButton;
